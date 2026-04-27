006736114a4261e1aba32fa3aad7568773b82049
DavidAvalos14 <angeldavidavaloscarrillo@gmail.com>
2026-04-27 12:28:58 -0600

    script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,16 @@
-# Copiamos código actualizado
+#!/bin/bash
+# Uso: bash deploy.sh
+
+echo 'Bajamos repositorio actualizado'
+# Copiamos código actualizado de GitHub
 git pull origin main
 
+echo 'Detenemos contenedores de la Aplicación'
 # Detenemos contenedores
 docker-compose down
 
+echo 'Inicializamos contenedores y reconstruimos imagen actualizada'
 # Iniciamos contenedores y reconstruimos
 docker-compose -f docker-compose.production.yml up -d --build
+
+echo 'Actualizacion finalizada'
